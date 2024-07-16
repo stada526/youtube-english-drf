440ae4d390bfb024f02571be1cf000b3bb2f7e22
stada526 <tsria526@gmail.com>
2024-07-16 18:17:05 -0400

    Update prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,9 @@
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://linopandemic.com"]
 
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["https://d7cu5e2dioocp.cloudfront.net/videos"]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ["https://d7cu5e2dioocp.cloudfront.net/videos"]
